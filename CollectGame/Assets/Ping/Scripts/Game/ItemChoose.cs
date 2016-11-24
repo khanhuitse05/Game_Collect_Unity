54e0df3db777af106d72a58f5b8419afda1e8583
Tô Ngọc Khánh <khanhuitse05@gmail.com>
2016-11-24 18:08:14 +0700

    -shop

---
+++ CollectGame/Assets/Ping/Scripts/Game/ItemChoose.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ItemChoose : MonoBehaviour {
+
+    public Image[] circle;
+    public Text txt;
+    public Image framer;
+    public Image mask;
+	public void Init() {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
